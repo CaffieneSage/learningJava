e3be7a190f3b03618fc893695b60c4ed6e392d99
CaffieneSage <jazznaz570@googlemail.com>
2022-05-12 09:38:30 +0100

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,31 @@
+public class helloWorld
+{
+    public static void main(String[] Args)
+    {
+        char[] letters = 
+        {
+            '\u0048',  //H
+            '\u0045',  //E
+            '\u004C',  //L
+            '\u004F',  //O
+            '\u0057',  //W
+            '\u0052',  //R
+            '\u0044',   //D
+            '\u0021'
+        };
+
+        System.out.print(letters[0]);
+        System.out.print(letters[1]);
+        System.out.print(letters[2]);
+        System.out.print(letters[2]);
+        System.out.print(letters[3]);
+        System.out.print(" ");
+        System.out.print(letters[4]);
+        System.out.print(letters[3]);
+        System.out.print(letters[5]);
+        System.out.print(letters[2]);
+        System.out.print(letters[6]);
+        System.out.print(letters[7]);
+
+    }    
+}
